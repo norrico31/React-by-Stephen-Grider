36be2ac645ae8b29a410305e16567cbb14ddb375
norrico31 <geraldbiasonjob31.com>
2020-08-05 17:08:54 +0800

    reusing a component

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,32 @@ import React from 'react'
 import faker from 'faker'
 
 import CommentDetail from './components/CommentDetail'
+import ApprovalCard from './components/ApprovalCard'
 
 const App = () => {
-    console.log(faker)
     return (
         <div className="ui container comments">
-            <CommentDetail author="Sam" timeAgo="Today at 4:45PM" avatar={faker.image.avatar()} content="Nice blog post!" />
-            <CommentDetail author="Alex" timeAgo="Today at 2:35PM" avatar={faker.image.avatar()} content="Keep up the good work" />
-            <CommentDetail author="Jane" timeAgo="Today at 1:00PM" avatar={faker.image.avatar()} content="You can do it!" />     
-            <CommentDetail author="Jessa" timeAgo="Today at 10:05M" avatar={faker.image.avatar()} content="Believe in yourself" />     
-            <CommentDetail author="Dhinalyn" timeAgo="Today at 8:35AM" avatar={faker.image.avatar()} content="Go for it!" />     
+            <ApprovalCard>
+                <div>
+                    <h4>Warning!</h4>
+                    Are you sure you want to do this?
+                </div>
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail author="Sam" timeA go="Today at 4:45PM" avzatar={faker.image.avatar()} content="Nice blog post!" />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail author="Alex" timeAgo="Today at 2:35PM" avatar={faker.image.avatar()} content="Keep up the good work" />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail author="Jane" timeAgo="Today at 1:00PM" avatar={faker.image.avatar()} content="You can do it!" />  
+            </ApprovalCard>   
+            <ApprovalCard>
+                <CommentDetail author="Jessa" timeAgo="Today at 10:05M" avatar={faker.image.avatar()} content="Believe in yourself" />  
+            </ApprovalCard>   
+            <ApprovalCard>
+                <CommentDetail author="Dhinalyn" timeAgo="Today at 8:35AM" avatar={faker.image.avatar()} content="Go for it!" /> 
+            </ApprovalCard>    
         </div>
     )
 }
