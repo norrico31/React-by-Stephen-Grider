4cb750309f863218e5a57d2ed53dbfd8bcadfc4d
norrico31 <geraldbiasonjob31.com>
2020-08-05 14:16:22 +0800

    created new components

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,15 @@
 import React from 'react'
 import faker from 'faker'
 
+import CommentDetail from './components/CommentDetail'
+
 const App = () => {
     console.log(faker)
     return (
         <div className="ui container comments">
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img src={faker.image.avatar()} alt="avatar"/>
-                </a>
-                <div className="content">
-                    <a href="/" className="author">
-                        Sam
-                    </a>
-                    <div className="metadata">
-                        <span className="date">Today at 6:00PM</span>
-                    </div>
-                    <div className="text">
-                        Nice blog post!
-                    </div>
-                </div>
-            </div>
+            <CommentDetail />
+            <CommentDetail />
+            <CommentDetail />     
         </div>
     )
 }
