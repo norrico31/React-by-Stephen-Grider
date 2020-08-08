0bd14b3895dad2565d16a37d90f1c7b1e1f07b4c
norrico31 <geraldbiasonjob31.com>
2020-08-08 19:19:11 +0800

    refactor class to function based component

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,50 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import SeasonDisplay from './SeasonDisplay'
 import Spinner from './Spinner'
 
-class App extends React.Component {
-    state = {
+const App = () => {
+    const [state, setState] = useState({
         lat: null,
         errorMessage: ''
-    }
+    })
+    const { lat, errorMessage } = state 
 
-    componentDidMount() {
+    useEffect(() => {
         console.log('My component was rendered')
         window.navigator.geolocation.getCurrentPosition(position => {
             console.log(position)
-            this.setState({ 
+            setState({ 
                 lat: position.coords.latitude,
                 errorMessage: null,
                 loading: false
             })
         }, err => {
-            this.setState({
+            setState({
                 errorMessage: err.message,
                 loading: false,
                 lat: null
             })
         })
+    }, [])
+
+    const renderContent = () => {
+        if (errorMessage && !lat) {
+            return <div>Error: {errorMessage}</div>
+        }
+        if (!errorMessage && lat) {
+            return <div><SeasonDisplay lat={lat} /></div>
+        }
+        return (
+            <div>
+                <Spinner message="Please accept location request" />
+            </div>
+        )
     }
 
-    componentDidUpdate() {
-        console.log('My component was just updated - it rerendered')
-    }
-
-    // React says we have to define render in class based component
-    render() {
-        if (this.state.errorMessage && !this.state.lat) {
-            return <div>Error: {this.state.errorMessage}</div>
-        }
-        if (!this.state.errorMessage && this.state.lat) {
-            return <div><SeasonDisplay lat={this.state.lat} /></div>
-        }
-        return <div><Spinner message="Please accept location request" /></div>
-    }
+    return (
+        <div className="border red">
+            {renderContent()}
+        </div>
+    )
 }
-
 export default App
